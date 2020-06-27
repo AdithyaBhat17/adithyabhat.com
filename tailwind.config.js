3c7276bbdf61a4a652b00d1cd3cd2dc1204ab958
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2020-06-28 00:56:00 +0530

    :sparkles: completed contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: ["./components/*.tsx", "./pages/*.tsx", "./pages/**/*.tsx"],
+  purge: ['./components/*.tsx', './pages/*.tsx', './pages/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         green: {
-          "500": "#50E3C2",
-          "600": "#29BC9B",
+          '500': '#50E3C2',
+          '600': '#29BC9B',
         },
       },
     },
   },
   variants: {},
   plugins: [],
-};
+}
