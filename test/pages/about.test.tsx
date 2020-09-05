8aef29c31db7f76ca6bc3f7b2295e6ae4e65acd5
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-09-05 17:26:39 +0530

    :wrench: fixed a broken test on about.tsx

--- test/pages/about.test.tsx
+++ test/pages/about.test.tsx
@@ -1,7 +1,8 @@
 import { render } from '../testUtils'
 import About from '@/pages/about'
+import { companies } from '@/utils/about'
 
 test('About page matches snapshot', () => {
-  const { container } = render(<About />)
+  const { container } = render(<About companies={companies} />)
   expect(container.innerHTML).toMatchSnapshot()
 })
