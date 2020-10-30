8fa7f044e2b3dedb9a0fa862d4427ba756726314
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-10-30 22:46:07 +0530

    :test_tube: Fixed broken tests

--- test/components/footer.test.tsx
+++ test/components/footer.test.tsx
@@ -1,5 +1,6 @@
 import { render } from '../testUtils'
-import Footer, { footerRoutes, socials } from '@/components/footer'
+import Footer, { footerRoutes } from '@/components/footer'
+import { socials } from '@/components/socials'
 
 test('renders all footer links', () => {
   const { getAllByTestId } = render(<Footer />)
