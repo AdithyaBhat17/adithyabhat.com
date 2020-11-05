cb06062814da51f3c3846c7a3ebb8e7c5824a009
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-11-05 22:21:11 +0530

    🎇Added custom hooks to animate on scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,9 @@ import { RECENT_WORK } from 'graphql/queries/work'
 import Container from '@/components/container'
 import Footer from '@/components/footer'
 import NextLink from '@/components/NextLink'
-import Link from 'next/link'
 import { motion } from 'framer-motion'
+import useResetScroll from 'hooks/useResetScroll'
+import Thanks from '@/components/thanks'
 
 const easing = [0.6, -0.05, 0.01, 0.99]
 export const fadeInUp = {
@@ -40,36 +41,28 @@ export const stagger = {
   },
 }
 
-export const Home = ({ recentWork, featuredArticles }): JSX.Element => (
-  <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
-    <Head title="Adithya NR | A Full stack Designer based in Bengaluru, India." />
-    <Navbar />
-    <Hero />
-    <Work data={recentWork} />
-    <Container>
-      <h1 className="mt-24 xl:mt-64 mb-6 text-2xl font-semibold">
-        Featured articles
-      </h1>
-      <ArticlesList data={featuredArticles} />
-      <NextLink href="/blog" text="Read more articles" />
-      <h1 className="mt-24 xl:mt-32 mb-6 text-3xl font-semibold">
-        Thanks for stopping by!
-      </h1>
-      <p className="poppins text-lg">
-        Got a project in mind, a question, or something else?{' '}
-        <br className="hidden lg:block" /> Get in touch with me{' '}
-        <Link href="/contact">
-          <a className="font-semibold hover:border-b-2 hover:border-blue-800">
-            here
-          </a>
-        </Link>
-      </p>
-    </Container>
-    <div className="mt-24 lg:mt-32">
-      <Footer />
-    </div>
-  </motion.div>
-)
+export const Home = ({ recentWork, featuredArticles }): JSX.Element => {
+  useResetScroll()
+  return (
+    <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
+      <Head title="Adithya NR | A Full stack Designer based in Bengaluru, India." />
+      <Navbar />
+      <Hero />
+      <Work data={recentWork} />
+      <Container>
+        <h1 className="mt-24 xl:mt-64 mb-6 text-2xl font-semibold">
+          Featured articles
+        </h1>
+        <ArticlesList data={featuredArticles} />
+        <NextLink href="/blog" text="Read more articles" />
+        <Thanks />
+      </Container>
+      <div className="mt-24 lg:mt-32">
+        <Footer />
+      </div>
+    </motion.div>
+  )
+}
 
 export async function getStaticProps() {
   const recentWork: WorkProps = await fetchAPI(RECENT_WORK)
