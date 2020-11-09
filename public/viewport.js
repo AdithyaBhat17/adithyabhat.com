c92ebea355918820d2c0a3a333949ca40404429b
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-11-10 00:04:49 +0530

    :bug: Avoid crashing viewport script if metadata is not found

--- public/viewport.js
+++ public/viewport.js
@@ -1,17 +1,19 @@
 const viewport = document.getElementById('viewport-meta')
-const viewports = {
-    default: viewport.getAttribute('content'),
-    landscape: 'width = 2560',
-    portrait: 'width = 768'
-}
-const setViewport = function () {
-    screen.width > 2560 ?
-        viewport.setAttribute('content', viewports.landscape) :
-        (screen.width >= 768 && screen.width < 1200) ?
-            viewport.setAttribute('content', viewports.portrait) :
-            viewport.setAttribute('content', viewports.default)
-}
-setViewport()
-window.onresize = function () {
+if (viewport) {
+    const viewports = {
+        default: viewport.getAttribute('content'),
+        landscape: 'width = 1920',
+        portrait: 'width = 768'
+    }
+    const setViewport = function () {
+        screen.width >= 2560 ?
+            viewport.setAttribute('content', viewports.landscape) :
+            (screen.width >= 768 && screen.width < 1200) ?
+                viewport.setAttribute('content', viewports.portrait) :
+                viewport.setAttribute('content', viewports.default)
+    }
     setViewport()
+    window.onresize = function () {
+        setViewport()
+    }
 }
