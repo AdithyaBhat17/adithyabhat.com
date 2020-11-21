f46fea122d1720f32a9e56f218b1707873b366ff
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-11-21 20:57:30 +0530

    :wrench: Created a single query to fetch featured work and articles

---
+++ graphql/queries/index.ts
@@ -0,0 +1,50 @@
+export const HOME_PAGE_QUERY = `
+    query HomePageQuery {
+        allProjects {
+            date
+            tags
+            slug
+            thumbnail {
+              alt
+              blurhash
+              title
+              responsiveImage(imgixParams: { fit: crop,  ar: "4:3", auto: format }) {
+                srcSet
+                webpSrcSet
+                sizes
+                src
+                width
+                height
+                aspectRatio
+                alt
+                title
+                base64
+              }
+            }
+            title
+        }
+        allArticles (filter: {featured: {eq: true}}, orderBy: date_DESC) {
+            date
+            tags
+            slug
+            thumbnail {
+              alt
+              blurhash
+              title
+              responsiveImage(imgixParams: {fit: crop, ar: "4:3", auto: format}) {
+                srcSet
+                webpSrcSet
+                sizes
+                src
+                width
+                height
+                aspectRatio
+                alt
+                title
+                base64
+              }
+            }
+            title
+        }
+    }
+`
