fba0296cf523d4c95bb50de5923e774bd3ec9a5b
adithyabhat17 <adithyabhat17.ab@gmail.com>
2020-11-21 20:58:01 +0530

    :wrench: Created a single query to fetch featured work and articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,38 +3,33 @@ import Head from '@/components/header'
 import Hero from '@/components/hero'
 import Navbar from '@/components/navbar'
 import Work from '@/components/work'
-import { AllArticles, WorkProps } from '@/interfaces/content'
+import { AllArticles, AllProjects, List } from '@/interfaces/content'
 import { fetchAPI } from '@/lib/datocms'
-import { FEATURED_ARTICLES } from 'graphql/queries/posts'
-import { RECENT_WORK } from 'graphql/queries/work'
 import Container from '@/components/container'
 import Footer from '@/components/footer'
 import NextLink from '@/components/NextLink'
 import { motion } from 'framer-motion'
 import useResetScroll from 'hooks/useResetScroll'
 import Thanks from '@/components/thanks'
+import { HOME_PAGE_QUERY } from 'graphql/queries'
 
-type HomeProps = {
-  recentWork: WorkProps
-  featuredArticles: AllArticles
-}
-
-export const Home = ({
-  recentWork,
-  featuredArticles,
-}: HomeProps): JSX.Element => {
+export const Home = ({ data }: List): JSX.Element => {
   useResetScroll()
   return (
     <motion.div initial="initial" animate="animate" exit={{ opacity: 0 }}>
       <Head title="Adithya NR | A Full stack Designer based in Bengaluru, India." />
       <Navbar />
       <Hero />
-      <Work data={recentWork} />
+      <Work data={data as AllProjects} />
       <Container>
         <h1 className="mt-24 xl:mt-64 mb-6 text-2xl font-semibold">
           Featured articles
         </h1>
-        <ArticlesList data={featuredArticles} type="allArticles" columns="3" />
+        <ArticlesList
+          data={data as AllArticles}
+          type="allArticles"
+          columns="3"
+        />
         <NextLink href="/blog" text="Read more articles" />
         <Thanks />
       </Container>
@@ -46,12 +41,10 @@ export const Home = ({
 }
 
 export async function getStaticProps() {
-  const recentWork: WorkProps = await fetchAPI(RECENT_WORK)
-  const articles: AllArticles = await fetchAPI(FEATURED_ARTICLES)
+  const data: List = await fetchAPI(HOME_PAGE_QUERY)
   return {
     props: {
-      recentWork,
-      featuredArticles: articles ?? [],
+      data,
     },
   }
 }
