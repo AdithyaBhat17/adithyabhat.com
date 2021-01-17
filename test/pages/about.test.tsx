704a81db4980cf85973f89c4a5bb9ab92fec4070
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2021-01-17 13:03:29 +0530

    🎂 wrote a util fn to calculate current age.

--- test/pages/about.test.tsx
+++ test/pages/about.test.tsx
@@ -1,10 +1,15 @@
 import { render } from '../testUtils'
 import About, { getStaticProps } from '@/pages/about'
 import { companies } from '@/utils/about'
+import { calculateAge } from '@/utils/age'
 
-test('About page matches snapshot', () => {
-  const { container } = render(<About companies={companies} />)
+test('About page matches snapshot and displays my age correctly', () => {
+  const age = calculateAge()
+  const { container, getByTestId } = render(
+    <About companies={companies} age={age} />
+  )
   expect(container.innerHTML).toMatchSnapshot()
+  expect(getByTestId('age').textContent).toBe(age.toString())
 })
 
 test('Get static props returns a list of companies', () => {
