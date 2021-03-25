030893c38f82c13324c68db596f91dd7e9f0d2ac
adithyabhat17 <adithyabhat17.ab@gmail.com>
2021-03-25 20:36:49 +0530

    ✨ Added micro-animations for the stack page

--- pages/stack.tsx
+++ pages/stack.tsx
@@ -3,6 +3,8 @@ import { Stack as IStack } from '../interfaces/use'
 import Head from '@/components/header'
 import Container from '@/components/container'
 import { useState, useMemo } from 'react'
+import { motion } from 'framer-motion'
+import { stagger } from '@/utils/motion'
 
 export default function Stack({ data, categories }) {
   const [selectedCategory, updateCategory] = useState<string | null>(null)
@@ -25,12 +27,17 @@ export default function Stack({ data, categories }) {
     <div>
       <Head title="Toolbox | Adithya NR" />
       <Container className="px-4">
-        <div className=" justify-start max-w-screen-sm">
-          <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-center my-10 flex-wrap sm:flex-nowrap">
+        <div className="justify-start max-w-screen-sm">
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ type: 'spring' }}
+            className="grid grid-cols-1 md:grid-cols-2 justify-between items-center my-10 flex-wrap sm:flex-nowrap"
+          >
             <h1 className="col-start-1 text-black font-semibold text-4xl">
               My Toolbox
             </h1>
-            <div className="md:col-start-13 bg-gray-100 px-4 py-2 mt-5 sm:mt-0 rounded-md">
+            <div className="w-max md:col-start-13 bg-gray-100 px-4 py-2 mt-5 sm:mt-0 rounded-md">
               <select
                 onChange={handleCategoryChange}
                 className="bg-gray-100 cursor-pointer"
@@ -46,10 +53,15 @@ export default function Stack({ data, categories }) {
                 })}
               </select>
             </div>
-          </div>
-          <div className="space-y-2">
-            {filteredStack?.map((stack: IStack) => (
-              <a
+          </motion.div>
+          <motion.div variants={stagger} className="space-y-2">
+            {filteredStack?.map((stack: IStack, index: number) => (
+              <motion.a
+                aria-label="product"
+                custom={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                layout
                 href={stack.link}
                 className="hover:bg-gray-50 flex items-start rounded-md px-2 py-4 sm:px-4"
                 rel="noopener noreferrer"
@@ -58,6 +70,9 @@ export default function Stack({ data, categories }) {
               >
                 {stack?.logo?.url ? (
                   <img
+                    width="50"
+                    height="50"
+                    loading="lazy"
                     src={stack.logo?.url}
                     className="stack-icon rounded-lg h-auto object-contain"
                   />
@@ -71,9 +86,9 @@ export default function Stack({ data, categories }) {
                     </span>
                   </div>
                 </div>
-              </a>
+              </motion.a>
             ))}
-          </div>
+          </motion.div>
         </div>
       </Container>
     </div>
@@ -100,6 +115,12 @@ export async function getStaticProps() {
 
   let categories = data.allStacks.map((product) => product.category)
   categories = Array.from(new Set([...categories]))
+
+  console.dir(
+    data.allStacks.filter(
+      (a) => a.category === 'MacOS' || a.category === 'Productivity'
+    )
+  )
 
   return {
     props: {
