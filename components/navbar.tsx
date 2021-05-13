7f200717fdbb1b89ff09a1c7ed493ef9ca58b43c
Adithya NR <adithyabhat17.ab@gmail.com>
2021-05-13 04:42:24 -0700

    :wrench: Fix: JS-0424, removed unnecessary Fragments

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -29,41 +29,39 @@ function Navbar() {
   const getRoute = () => useRouter()?.pathname?.split('/')[1] ?? null
 
   return (
-    <>
-      <nav className="navbar px-6 md:px-36 lg:px-48">
-        <div className="flex justify-start items-center">
-          <Link href="/">
-            <a className="hover:border-white">
-              <img
-                className="fill-current lg:h-16 lg:w-16 w-12 h-12"
-                src="/static/logo_black.svg"
-                alt="Adithya NR logo"
-              />
-            </a>
-          </Link>
-          <span className="sm:inline hidden ml-6 text-gray-500 capitalize">
-            {getRoute()}
-          </span>
-        </div>
+    <nav className="navbar px-6 md:px-36 lg:px-48">
+      <div className="flex justify-start items-center">
+        <Link href="/">
+          <a className="hover:border-white">
+            <img
+              className="fill-current lg:h-16 lg:w-16 w-12 h-12"
+              src="/static/logo_black.svg"
+              alt="Adithya NR logo"
+            />
+          </a>
+        </Link>
+        <span className="sm:inline hidden ml-6 text-gray-500 capitalize">
+          {getRoute()}
+        </span>
+      </div>
 
-        <ul className="flex justify-between">
-          {routes?.map((route, i) => (
-            <li className="ml-6" key={i}>
-              <Link href={`${route.link}`}>
-                <a
-                  data-testid="nav-link"
-                  className={`${activeStyle(
-                    route.link
-                  )} text-black text-sm hover:border-gray-900`}
-                >
-                  {route.name}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
+      <ul className="flex justify-between">
+        {routes?.map((route, i) => (
+          <li className="ml-6" key={i}>
+            <Link href={`${route.link}`}>
+              <a
+                data-testid="nav-link"
+                className={`${activeStyle(
+                  route.link
+                )} text-black text-sm hover:border-gray-900`}
+              >
+                {route.name}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
