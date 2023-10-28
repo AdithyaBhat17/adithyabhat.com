ed34128623dc9c9f8b77093755d7542af3fe9f7a
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2023-10-28 21:19:21 +0530

    fix broken tests

--- test/pages/photos.test.tsx
+++ test/pages/photos.test.tsx
@@ -1,11 +1,6 @@
 import PhotosPage from '@/pages/photos'
 import { render } from '@testing-library/react'
-import { mockProjects } from 'test/__mocks__/content'
-
-// Mock the fetchGalleryAPI function
-jest.mock('../lib/datocms', () => ({
-  fetchGalleryAPI: jest.fn().mockResolvedValue({ /* mock data here */ }),
-}))
+import { mockProjects } from '../__mocks__/content'
 
 describe('PhotosPage', () => {
   it('renders without crashing', () => {
