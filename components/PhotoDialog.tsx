03ba47160414b633fc8a8a6cde02a17733d233ef
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2023-10-28 21:42:15 +0530

    close icon overlap

--- components/PhotoDialog.tsx
+++ components/PhotoDialog.tsx
@@ -38,7 +38,7 @@ export function PhotoDialog({ open, title, description, close, image }) {
               <Dialog.Panel className="w-full max-w-[100vw] h-[100dvh] transform overflow-y-auto bg-white text-left align-middle shadow-xl transition-all">
                 <motion.div variants={stagger} className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 items-center h-full gap-y-4">
                   <motion.div initial={{opacity: 0}} animate={{opacity: 1}} className="col-span-1 overflow-hidden sm:col-span-1 lg:col-span-3 h-full items-center bg-gray-50 p-6">
-                    <motion.figure variants={width < 600 ? fadeInDown : fadeInRight} className="w-full h-full my-auto flex flex-col items-center mt-10 sm:mt-0">
+                    <motion.figure variants={width < 600 ? fadeInDown : fadeInRight} className="w-full h-full my-auto flex flex-col items-center">
                       <img
                         src={image.src}
                         alt={title}
@@ -55,7 +55,7 @@ export function PhotoDialog({ open, title, description, close, image }) {
                   </motion.div>
                   <div className="flex flex-col justify-center items-center w-full col-span-1">
                     <button
-                      className="text-blackA8 hover:bg-blackA3 absolute top-[10px] right-[10px] inline-flex h-[48px] w-[48px] appearance-none items-center justify-center rounded-full p-2 focus:outline-none"
+                      className="text-blackA8 bg-white focus:shadow-2xl hover:bg-blackA3 absolute top-[10px] right-[10px] inline-flex h-[48px] w-[48px] appearance-none items-center justify-center rounded-full p-2 focus:outline-none"
                       aria-label="Close"
                       onClick={close}
                     >
